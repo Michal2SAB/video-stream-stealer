34934ea134fd5278117b6d66b06cb8cdd5e3888f
Michał <38110165+Michal2SAB@users.noreply.github.com>
2021-11-07 19:18:35 +0100

    Update run.bat
    Now run.bat does everything for you.

--- run.bat
+++ run.bat
@@ -1,1 +1,22 @@
+@echo off
+title Video Stream Stealer @Michal2SAB
+
 node download.js
+
+ffmpeg -f concat -i segments.txt -c copy tsvideo.ts
+
+echo.
+
+echo Attempting to convert final .ts file to .mp4 video..
+
+echo.
+
+ffmpeg -i tsvideo.ts -acodec copy -vcodec copy mp4video.mp4
+
+echo.
+
+echo Success! Created brand new .mp4 video for you. Enjoy and thank Michal2SAB lol
+
+echo.
+
+pause
